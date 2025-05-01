fde4271d37f2cf5da3650943b5bb9f0d69decb18
rameezwangde <137885331+rameezwangde@users.noreply.github.com>
2025-05-01 16:29:58 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,16 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int sum = a + b;
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
+// Go to jenkins click freestyle project
+// Click "Add build step"
+
+
+// Choose "Execute Windows batch command"
+// cd "C:\Users\RAMEEZ\OneDrive\Desktop\Devops" - check this in vs code terminal
+// javac Addition.java
+// java Addition
+
